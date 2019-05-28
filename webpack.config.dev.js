0a5632051b9dce4bd2823aa3783079fa09332c8a
Steve Onyeneke <sonyeneke@Steves-MacBook-Pro.local>
2019-05-28 14:36:25 +0100

    Setup development environment
    - Install necessary react/redux & test dependencies
    - Create index.html
    - Create index.js
    - Configure webpack.config.dev file
    - Configure babel in package.json
    - Add npm start script in package.json
    - Configure eslint in package.json

---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  // source map lets us see our original code when debugging in the browser
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    // Reduce the info written to cmdline
+    stats: "minimal",
+    // Overlay any errors that occur in the browser
+    overlay: true,
+    // All request will be sent to index.html. Thus we can load deep-links to be handled by react-router
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
