19662f24f2733f191be51d22635948a571253897
Steve Onyeneke <sonyeneke@Steves-MacBook-Pro.local>
2019-05-31 16:28:13 +0100

    Display course titles via mock api
    - Use redux thunk middleware

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,16 @@
 import * as types from "./actionTypes"
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getCourses().then(courses => {
+      dispatch({ type: types.LOAD_COURSES_SUCCESS, courses });
+    }).catch(err => {
+      throw err;
+    })
+  }
 }
